ce34cdca5749e794e3182e0c24c0922be6dd8098
Mitali sharma <abhijithp05@gmail.com>
2020-10-19 00:45:14 +0530

    Updates

---
+++ precache-manifest.95c311858a02e1d879bac8f1d75b9284.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74f0299a72bc5e4c856a8e77fea36f12",
+    "url": "/bidOnHomes/index.html"
+  },
+  {
+    "revision": "8722c95d75e6543e61fe",
+    "url": "/bidOnHomes/static/css/main.47feb0b4.chunk.css"
+  },
+  {
+    "revision": "0bf929c061100fcc3411",
+    "url": "/bidOnHomes/static/js/2.d5626775.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/bidOnHomes/static/js/2.d5626775.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8722c95d75e6543e61fe",
+    "url": "/bidOnHomes/static/js/main.409f7daa.chunk.js"
+  },
+  {
+    "revision": "30c78ea4a344263662dc",
+    "url": "/bidOnHomes/static/js/runtime-main.0983724d.js"
+  }
+]);
